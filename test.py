cf68ade974c21ed0a90bdd817fcf9c408fa2cf8e
kangchanguk <rkdckddnr01@naver.com>
2019-11-27 00:15:22 +0900

    te

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
+import keras
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, BatchNormalization
 from tensorflow.keras.datasets.cifar10 import load_data
 
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
-import keras
+
 import numpy as np
 #from tensorflow.keras import datasets, layers, models
 
@@ -24,36 +25,44 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 
 model = Sequential([
     Conv2D(32, 3, padding='same', activation='relu', input_shape=(32, 32 ,3)),
+    BatchNormalization(),
     Conv2D(32, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     MaxPooling2D(),
     Dropout(0.2),
     Conv2D(64, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     Conv2D(64, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     MaxPooling2D(),
     Dropout(0.2),
     Conv2D(128, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     Conv2D(128, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     MaxPooling2D(),
     Dropout(0.2),
     Conv2D(256, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     Conv2D(256, 3, padding='same', activation='relu'),
+    BatchNormalization(),
     MaxPooling2D(),
     Flatten(),
     Dense(128, activation='relu'),
     Dropout(0.2),
     Dense(10, activation='softmax')
 ])
+datagen = ImageDataGenerator(rotation_range=15,width_shift_range=0.1, height_shift_range = 0.1, horizontal_flip =True)
+datagen.fit(train_images)
+it_train = datagen.flow(train_images,train_labels,64)
+opt=tf.keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9)
 
-datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range = 0.1, horizontal_flip =True)
-it_train = datagen.flow(train_images,train_labels,batch_size=64)
-
-
-model.compile(optimizer="adam",
+model.compile(optimizer=opt,
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 steps =int(train_images.shape[0]/64)
-model.fit_generator(it_train, steps_per_epoch=steps,epochs=10, validation_data=(test_images,test_labels),verbose=0)
+model.fit_generator(it_train, steps_per_epoch=train_images.shape[0]//64,epochs=125,verbose=1, validation_data=(test_images,test_labels),)
 #model.fit(train_images,train_labels,epochs=10)
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 print(test_acc)
