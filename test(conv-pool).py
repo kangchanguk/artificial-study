c0ec7c14662c62267908ee9a5e6c827277bafdcb
kangchanguk <rkdckddnr01@naver.com>
2019-11-28 21:15:20 +0900

    85.392(epoch250)

--- test(conv-pool).py
+++ test(conv-pool).py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import os
-import keras
-import tensorflow as tf
 
+import tensorflow as tf
+import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Convolution2D, GlobalAveragePooling2D
 from tensorflow.keras.datasets.cifar10 import load_data
+
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -22,28 +22,23 @@ test_images = test_images.reshape((10000, 32, 32, 3))
 # 픽셀 값을 0~1 사이로 정규화합니다.
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-if os.path.isfile("cifar-10cnn_model.h5"):
-    model=tf.keras.models.load_model('cifar-10cnn_model.h5')
-
-
-
-else:
-    model = Sequential([
-        Conv2D(36, 3, padding='same', activation='relu', input_shape=(32, 32 ,3)),
-        Conv2D(36, 3, padding='same', activation='relu'),
-        Conv2D(36, 3, strides=2, padding='same', activation='relu'),
-        Conv2D(64, 3, padding='same', activation='relu'),
-        Dropout(0.2),   
-        Conv2D(64, 3, padding='same', activation='relu'),
-        Conv2D(64, 3, padding='same', activation='relu'),
-        Conv2D(64, 3, strides=2,padding='same', activation='relu'),
-        Dropout(0.5),
-        Conv2D(128, 3, padding='same', activation='relu'),
-        Conv2D(128, 1, padding='same', activation='relu'),
-        Conv2D(10, 1, padding='same', activation='relu'),
-        GlobalAveragePooling2D(),
-        Dense(10, activation='softmax')
-    ])
+model = Sequential([
+    Conv2D(48, 3, padding='same', activation='relu', input_shape=(32, 32 ,3)),
+    Conv2D(48, 3, padding='same', activation='relu'),
+    Conv2D(48, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Dropout(0.2),   
+    Conv2D(96, 3, padding='same', activation='relu'),
+    Conv2D(96, 3, padding='same', activation='relu'),
+    Conv2D(96, 3, padding='same', activation='relu'),
+    MaxPooling2D()
+    Dropout(0.5),
+    Conv2D(96, 3, padding='same', activation='relu'),
+    Conv2D(96, 1, padding='same', activation='relu'),
+    Conv2D(10, 1, padding='same', activation='relu'),
+    GlobalAveragePooling2D(),
+    Dense(10, activation='softmax')
+])
 
 datagen = ImageDataGenerator(rotation_range=15,width_shift_range=0.1, height_shift_range = 0.1, horizontal_flip =True)
 datagen.fit(train_images)
@@ -55,9 +50,7 @@ model.compile(optimizer=opt,
               metrics=['accuracy'])
 
 
-model.fit_generator(it_train, steps_per_epoch=train_images.shape[0]//64,epochs=100,verbose=1, validation_data=(test_images,test_labels),)
+model.fit_generator(it_train, steps_per_epoch=train_images.shape[0]//64,epochs=150,verbose=1, validation_data=(test_images,test_labels),)
 #model.fit(train_images,train_labels,epochs=50)
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
 print(test_acc)
-model.save('cifar-10cnn_model.h6')
--- test12.py
+++ test12.py
@@ -1,79 +1,57 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-#baseline model with data augmentation on the cifar10 dataset
-import sys
-from keras.datasets import cifar10
-from keras.utils import to_categorical
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.optimizers import SGD
+import tensorflow as tf
+import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Convolution2D, GlobalAveragePooling2D
+from tensorflow.keras.datasets.cifar10 import load_data
+
+from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
- 
-# load train and test dataset
-def load_dataset():
-	# load dataset
-	(trainX, trainY), (testX, testY) = cifar10.load_data()
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-	return trainX, trainY, testX, testY
- 
-# scale pixels
-def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	# return normalized images
-	return train_norm, test_norm
- 
-# define cnn model
-def define_model():
-	model = Sequential()
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
-	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	model.add(MaxPooling2D((2, 2)))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-	model.add(Dense(10, activation='softmax'))
-	# compile model
-	opt = SGD(lr=0.001, momentum=0.9)
-	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-	return model
- 
-# plot diagnostic learning curves
 
- 
-# run the test harness for evaluating a model
-def run_test_harness():
-	# load dataset
-	trainX, trainY, testX, testY = load_dataset()
-	# prepare pixel data
-	trainX, testX = prep_pixels(trainX, testX)
-	# define model
-	model = define_model()
-	# create data generator
-	datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
-	# prepare iterator
-	it_train = datagen.flow(trainX, trainY, batch_size=64)
-	# fit model
-	steps = int(trainX.shape[0] / 64)
-	history = model.fit_generator(it_train, steps_per_epoch=steps, epochs=100, validation_data=(testX, testY), verbose=0)
-	# evaluate model
-	_, acc = model.evaluate(testX, testY, verbose=0)
-	print('> %.3f' % (acc * 100.0))
-	# learning curves
-	
- 
-# entry point, run the test harness
-run_test_harness()
+import numpy as np
+#from tensorflow.keras import datasets, layers, models
+
+#(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = load_data()
+
+train_images = train_images.reshape((50000, 32, 32, 3))
+
+test_images = test_images.reshape((10000, 32, 32, 3))
+
+# 픽셀 값을 0~1 사이로 정규화합니다.
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+model = Sequential([
+    Conv2D(96, 3, padding='same', activation='relu', input_shape=(32, 32 ,3)),
+    Conv2D(96, 3, padding='same', activation='relu'),
+    Conv2D(96, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(192, 3, padding='same', activation='relu'),
+    Dropout(0.2),   
+    Conv2D(192, 3, padding='same', activation='relu'),
+    Conv2D(192, 3, padding='same', activation='relu'),
+    Conv2D(192, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Dropout(0.2),
+    Conv2D(192, 3, padding='same', activation='relu'),
+    Conv2D(192, 1, padding='same', activation='relu'),
+    Conv2D(10, 1, padding='same', activation='relu'),
+    GlobalAveragePooling2D(),
+    Dense(10, activation='softmax')
+])
+
+datagen = ImageDataGenerator(rotation_range=15,width_shift_range=0.1, height_shift_range = 0.1, horizontal_flip =True)
+datagen.fit(train_images)
+it_train = datagen.flow(train_images,train_labels,64)
+opt=tf.keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9)
+
+model.compile(optimizer=opt,
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+steps =int(train_images.shape[0]/64)
+model.fit_generator(it_train, steps_per_epoch=train_images.shape[0]//64,epochs=10,verbose=1, validation_data=(test_images,test_labels),)
+#model.fit(train_images,train_labels,epochs=10)
+test_loss, test_acc = model.evaluate(test_images,  tetst_labels, verbose=2)
+print(test_acc)
